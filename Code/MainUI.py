6244b98e27e9d09dc56ef51aff239439169529ed
Field <inemiolts@outlook.com>
2026-07-05 17:09:37 +0800

    更新头像

--- Code/MainUI.py
+++ Code/MainUI.py
@@ -5,18 +5,41 @@ def main(page: ft.Page):
     page.padding=0
 
 
+    fake_users = ["星野", "李四", "王五"]
+
+    user_tiles = []
+    for name in fake_users:
+        # 这里假设你的图片文件名和用户名对应，比如 "张三.png"
+        avatar = ft.CircleAvatar(
+            foreground_image_src=f"Image/{name}.png",
+            radius=20,  # 设置小一点，适合列表
+        )
+        user_tiles.append(
+            ft.ListTile(
+                leading=avatar,
+                title=ft.Text(name),
+            )
+        )
+    # ListView 用 expand=True 撑满父容器的高度
+    left_list = ft.ListView(
+        controls=user_tiles,
+        spacing=0,
+        padding=10,
+        expand=True,  # 高度自动填满 left_panel
+        width=175
+    )
+
     left_panel = ft.Container(
-        content=ft.Text("左边导航", size=20, color=ft.Colors.BLACK),
-        width=200,  # 固定宽度
-        bgcolor=ft.Colors.GREY_300,  # 灰色背景
-        alignment=ft.alignment.Alignment.CENTER,  # 让文字居中
+        content=left_list,
+        bgcolor=ft.Colors.WHITE_12,
+        alignment=ft.Alignment.CENTER, # 让文字居中
     )
 
 # 2. 右边：就是一个白色方块，里面写个“右”
     right_panel = ft.Container(
         content=ft.Text("右边聊天区", size=20),
         bgcolor=ft.Colors.WHITE,
-        alignment=ft.alignment.Alignment.CENTER,
+        alignment=ft.Alignment.CENTER,
         expand=True,
     )
     row_layer=ft.Row(
